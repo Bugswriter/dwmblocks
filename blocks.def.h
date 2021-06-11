d18d5b9c4f0ed77b77f9d17c62d0628e2345fee9
Suraj Kushwah <suraj@bugswriter.com>
2021-06-11 19:37:20 +0530

    removing some useless scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,15 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-  {"",  "nettraf",  1,  16},
-  {"",  "price eth Ethereum 💎",  9000, 23}, 
-  {"",  "cpu",    10, 18}, 
+/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+  {"",  "nettraf",  1,  16}, 
   {"",  "memory", 10,  14},
   {"",  "volume", 1,  10},
   {"",  "clock",  60, 1},
   {"",  "internet", 5,  4},
-  {"",  "battery",  5,  3},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
